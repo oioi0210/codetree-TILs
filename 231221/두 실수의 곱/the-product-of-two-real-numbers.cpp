55b19a76ff34e68df41a8e494242952894963ea3
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2023-12-21 01:31:59 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231221/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+   double a = 5.26;
+   double b = 8.27;
+
+   cout.precision(3);
+   cout<< a * b ;
+    return 0;
+}

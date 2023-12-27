a842441e5d26e4b1f91e900021bed6f3cf459064
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2023-12-27 20:50:32 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   double a,b;
+
+  
+   
+cin >> a;
+cin >> b;
+ cout << fixed;
+ cout.precision(2);
+   cout << (a+b)/ (a-b) ;
+
+
+    return 0;
+}

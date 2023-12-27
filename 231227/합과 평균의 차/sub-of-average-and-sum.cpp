291241a5043b89705d3c23c525b7390f6aa29872
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2023-12-27 21:05:31 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a,b,c;
+    cin >> a;
+      cin >> b;
+        cin >> c;
+
+
+	cout << a + b+c << endl <<(a + b+c) / 3 << endl << (a+b+c) - (a+b+c) /3;
+
+	return 0;
+
+}

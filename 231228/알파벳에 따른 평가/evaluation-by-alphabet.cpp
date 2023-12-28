881dd9ab73002f8f6db6b4a4eea91d05352d703d
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2023-12-28 21:22:04 +0900

    Title: 알파벳에 따른 평가, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 231228/알파벳에 따른 평가/evaluation-by-alphabet.cpp
+++ 231228/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -4,22 +4,22 @@ using namespace std;
 int main() {
 
 	char a;
-	
+	scanf("%c", &a);
 
 	if (a == 'S') {
-		cout << "Superior" << endl;
+		cout << "Superior" ;
 	}
 	else if (a == 'A') {
-		cout << "Excellent" << endl;
+		cout << "Excellent";
 	}
 	else if (a == 'B') {
-		cout << "Good" << endl;
+		cout << "Good" ;
 	}
-	else if (a ='C') {
-		cout << "Usually" << endl;
+	else if (a == 'C') {
+		cout << "Usually" ;
 	}
-    else if (a = 'D') {
-		cout << "Effort" << endl;
+    else if (a == 'D') {
+		cout << "Effort"  ;
 	}
     else {
         cout << "Failure";

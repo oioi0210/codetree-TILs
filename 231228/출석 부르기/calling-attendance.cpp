89c21693ad4d7031636d90a11d8e5795b2c67d32
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2023-12-28 20:27:14 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a;
+	cin >> a;
+
+	if (a == 1) {
+		cout << "John" << endl;
+	}
+	else if (a ==2) {
+		cout << "Tom" << endl;
+	}
+	else if (a==3) {
+		cout << "Paul" << endl;
+	}
+	else {
+		cout << "Vacancy" << endl;
+	}
+
+
+	return 0;
+
+}

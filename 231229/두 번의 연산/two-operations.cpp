c9e8ad9517c705efbd437bca4cfceedb6296eb9c
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2023-12-29 22:41:06 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231229/두 번의 연산/two-operations.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a;
+
+	cin >> a;
+
+	if (a % 2 != 0) {
+		a=a+3;
+	}
+
+	if (a%3 ==0) {
+		cout << a/3<< endl;
+	}
+   
+
+
+	return 0;
+
+}

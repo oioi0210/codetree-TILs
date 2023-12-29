361fead76571900b85eacc988b45f1b14a8c9e48
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2023-12-29 22:36:25 +0900

    Title: 최대 2번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231229/최대 2번의 연산/up-to-2-calculations.cpp
+++ 231229/최대 2번의 연산/up-to-2-calculations.cpp
@@ -4,19 +4,17 @@ using namespace std;
 int main() {
 
 	int a;
-int b;
+
 	cin >> a;
 
-	if (a >=2 ,a % 2 != 0) {
-	 cout << a/2;
-	  
+	if (a % 2 == 0) {
+		a=a/2;
 	}
 
- else if   (a==1) {cout << a;}
-b=a/2;
-	if (b %2 == 1) {
-		cout << (b+1)/2<< endl;
+	if (a %2==1) {
+		cout << (a+1) /2<< endl;
 	}
+    else {cout << a ;}
 
 
 

4178d193eb069228a14977ef7b3589aef37d352c
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2023-12-30 22:45:50 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231230/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a,b,c,d,e;
+
+	cin >> a;
+	cin>> b;
+    cin>> c;
+    cin>> d;
+    cin>> e;
+cout << (a >b) << endl;
+cout << (a > c) << endl;
+cout << (a >d) << endl;
+cout << (a > e) << endl;
+
+}

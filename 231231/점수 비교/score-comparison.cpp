3be6bfc4138a61a123738f71264233d0087fee33
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2023-12-31 01:14:56 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231231/점수 비교/score-comparison.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a,b,c,d;
+	cin >> a;
+    cin >>b;
+	cin >> c;
+	cin >> d;
+
+if (a > c && b > d) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+
+
+	return 0;
+
+}

5a98be7d5c0ff8e7cf94d312fb3f298cd7e9ac02
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-02 00:10:53 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/두 사람/two-person.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,c;
+    char b,d;
+    cin >> a;
+    cin >>b;
+      cin >>c;
+        cin >>d;
+
+   if (a>=19 && b=='M' ) {
+		cout << "1" << endl;
+	}
+    else if  (c>=19 && d=='M' ) {
+		cout << "1" << endl;
+	}
+
+	else {
+		cout << "0" << endl;
+	}
+  
+    return 0;
+}

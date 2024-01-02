705fe67c641fc5860b6b2558972d489e375ff033
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-02 22:43:41 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240102/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a,c,e;
+    int b,d,f;
+cin >> a;
+cin >> b;
+cin >> c;
+cin >> d;
+cin >> e;
+cin >> f;
+
+
+    if (a=='Y' && b >=37 ) {
+        if((c=='Y' && d >= 37) ||  (e=='Y' && f >= 37)){
+        cout << "E";
+        }
+    
+          
+        }
+        
+     if (c=='Y' && d >= 37) {
+        if (e=='Y' && f >= 37){
+            cout << "E";
+        }
+        }
+         
+        
+        return 0;
+        
+    }

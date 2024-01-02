42d678018b053e8bdd1dce196741dec6ecfcc901
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-02 20:42:57 +0900

    Title: 최댓값 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+if (a >= b && a>=c) {
+    cout << a;
+}
+else if (b >= c && b>=a) {
+    cout << b;
+}
+else  {
+    cout << c;
+}
+       // 여기에 코드를 작성해주세요.
+    return 0;
+}

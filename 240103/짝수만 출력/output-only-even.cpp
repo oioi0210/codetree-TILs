305a417162f4c60114124b5e5186339919a2ab15
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-03 18:38:26 +0900

    Title: 짝수만 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a,b;
+cin >>a;
+cin >> b;
+
+	int i = a;
+
+	while (i <= b) {
+		cout << i << " ";
+		i += 2;
+	}
+
+
+	return 0;
+
+}

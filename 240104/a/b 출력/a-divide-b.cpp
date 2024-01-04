602e93dbdc62dbff195cc947503542a0629f6003
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-04 17:36:04 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240104/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double a,b;
+	cin >> a;
+	cin >> b;
+
+
+cout << a/b;
+	for (int i = 1; i <= 19; i++) {
+		cout << 0 ;
+	}
+
+	return 0;
+
+}

163b7fef1e9d25c23626f9be6a567b026dc2207c
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-05 22:12:08 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/a 부터 b 까지/a-to-b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a,b;
+cin >>a>>b;
+
+	for (int i = a; i >=b;i++ ) {
+		if (a%2 == 1) {
+			cout << 2 * a << " ";
+			
+		}
+		
+		else   {
+			cout << (a+3) << " " ;
+		
+    }
+
+    }
+	
+	return 0;
+
+}

ef6e0b6a3bb0e355f41673f7f2150607aa926840
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-05 22:40:55 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/369 게임/369-game.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int n;
+
+	// 입력
+	cin >> n;
+	
+	
+    
+    // 출력
+	for(int i=1; i <= n; i++) {
+	if ((i%10 ==3) || (i%10 ==6)) {
+		cout << 0 << " ";
+	}
+	else if ( i %10 == 9) {
+		cout << 0 << " ";
+
+	}
+	else {
+		cout << i << " ";
+
+	}
+
+	}
+    return 0;
+}

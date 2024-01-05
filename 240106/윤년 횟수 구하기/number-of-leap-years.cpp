bbf6b3ddcf637167ac289537cd0dc83cfe9d2472
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-06 01:54:12 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240106/윤년 횟수 구하기/number-of-leap-years.cpp
+++ 240106/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -7,7 +7,7 @@ int main() {
     int n;
     cin >> n;
     int cnt= 0;
-    for(int i = 1; i < n; i++) {
+    for(int i = 1; i < n+1; i++) {
     
      if (i%100==0 && i % 4 == 0 && i%400==0) {
              cnt++;

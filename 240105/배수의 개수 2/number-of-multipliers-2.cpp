d988890872193b89a4cc9144ef4fe1b5c3bd4f09
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-05 23:56:59 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	
+	int cnt = 0;
+
+	for (int i = 1; i <= 10; i++) {
+        int n;
+        cin >> n;
+		if (i % 2 == 1) {
+			cnt++;
+		}
+	}
+
+	cout << cnt;
+	return 0;
+
+}

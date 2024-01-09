0f0746af9fc8307623319fae899ca34dcc7c8d16
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-09 18:34:39 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240109/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	int sum_val = 0;
+
+	while (true) {
+		cin >> n;
+		
+		if (n == 0) {
+			
+			break;
+		}
+	
+		cout  << n << endl;
+	}
+	
+
+	return 0;
+
+}

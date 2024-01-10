f550142859eeabe85435954017225aa290270efc
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-10 20:14:33 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240110/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
+++ 240110/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -9,15 +9,15 @@ int main() {
 
 	bool satisfied = false;
 	for (int i = a; i <= b; i++) {
-		if (1920%i != 0&& 2880%i !=0) {
+		if (1920%i == 0&& 2880%i ==0) {
 			satisfied = true;
 		}
 	}
 	if (satisfied == true) {
-		cout << 0;
+		cout << 1;
 	}
 	else {
-		cout << 1;
+		cout << 0;
 	}
 
 	return 0;

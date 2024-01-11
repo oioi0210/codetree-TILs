f95a3942b0766b355be6b55ed5216d2db9cdae1f
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-11 19:35:55 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a, b,c;
+	cin >> a >> b>>c;
+
+	bool satisfied = true;
+	for (int i = a; i <= b; i++) {
+		if (i % c == 0) {
+			satisfied = false;
+		}
+	}
+	if (satisfied == true) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+
+	return 0;
+
+}

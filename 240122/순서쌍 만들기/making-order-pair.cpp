f963f3bf0c2cf230dcea7f3896b03c06fa799a61
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-22 20:37:13 +0900

    Title: 순서쌍 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240122/순서쌍 만들기/making-order-pair.cpp
+++ 240122/순서쌍 만들기/making-order-pair.cpp
@@ -6,8 +6,9 @@ int n;
 cin >> n;
 
 	for (int i = n; i > 0; i--) {
-		for (int j = 0; j < n; j++) {
-			cout << "(" << i << ","<< i<< ")";
+		for (int j = n; j>0; j--) {
+			
+			cout << "(" << i << ","<< j<< ")" << " ";
 		}
 		cout << endl;
 	}

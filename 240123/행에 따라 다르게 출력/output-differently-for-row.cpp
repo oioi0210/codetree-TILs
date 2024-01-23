c2e01f14ad205728158cd05408e632a90ae1dc07
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-23 21:52:48 +0900

    Title: 행에 따라 다르게 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/행에 따라 다르게 출력/output-differently-for-row.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n;
+cin >> n;
+int cnt =0;
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 != 0) {
+			for (int j = 1; j <= n; j++) {
+				cnt++;
+				cout << cnt<< " ";
+
+				
+			}
+		}
+		else {
+			
+			for (int j = 0; j < n; j++) {
+				cnt+=2;
+				cout << cnt<< " ";
+				
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
+
+}

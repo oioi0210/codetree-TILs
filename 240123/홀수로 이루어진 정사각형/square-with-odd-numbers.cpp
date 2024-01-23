bb915c0bb12908834eb1521212b785151e21a7eb
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-23 17:37:41 +0900

    Title: 홀수로 이루어진 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240123/홀수로 이루어진 정사각형/square-with-odd-numbers.cpp
+++ 240123/홀수로 이루어진 정사각형/square-with-odd-numbers.cpp
@@ -6,7 +6,7 @@ int n;
 cin >> n;
 
 	for (int i = 1; i <= n; i++) {
-		for (int j = 9+2*i; j <= 17+2*i; j+=2) {
+		for (int j =9+2*i ; j <=11+2*i+2*n-4; j+=2) {
 			cout <<  j<< " ";
 		}
 		cout << endl;

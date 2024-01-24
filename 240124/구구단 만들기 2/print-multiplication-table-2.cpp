6d67f9848843cea503bd4b67d6a76945a6ba0414
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-24 14:23:37 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/구구단 만들기 2/print-multiplication-table-2.cpp
+++ 240124/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -5,9 +5,9 @@ int main() {
 int a,b;
 cin >>a >> b;
 
-	for (int i = 1; i <= b; i++) {
+	for (int i = 1; i <= 4; i++) {
 		for (int j = b; j >= a; j--) {
-			cout << j << " * " << a*i << " = " << j*a*i << " ";
+			cout << j << " * " << 2*i << " = " << j*2*i << " ";
 			if (j > a) cout << "/ ";
 		}
 		cout << endl;

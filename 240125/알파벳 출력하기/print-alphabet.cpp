5048d37e321fb34ab186217e1170061ea055bf7b
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-25 20:45:05 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/알파벳 출력하기/print-alphabet.cpp
+++ 240125/알파벳 출력하기/print-alphabet.cpp
@@ -10,9 +10,9 @@ for (int i =1; i<=n; i++) {
 for (int j =1; j<=i; j++ ){
     cout << (char) x ;
     x++;
-    if (x >25) {
-        char x='A';
-        x=1;
+    if (x =='[') {
+        
+        x='A';
     }
 }
 cout << endl;

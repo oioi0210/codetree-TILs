dfc7105decfb734453ba58c02d1369409fe18718
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-25 20:38:58 +0900

    Title: 알파벳 출력하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/알파벳 출력하기/print-alphabet.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+    char x='A';
+for (int i =1; i<=n; i++) {
+for (int j =1; j<=i; j++ ){
+    cout << (char) x ;
+    x++;
+}
+cout << endl;
+}
+
+
+
+	return 0;
+}

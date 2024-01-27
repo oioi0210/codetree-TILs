7fa38bb927d6f7fcca56f337d23482be1a7cf908
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-27 21:54:14 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240127/소수 구하기/get-prime.cpp
+++ 240127/소수 구하기/get-prime.cpp
@@ -9,19 +9,19 @@ int main() {
 	int cnt=0;
    
 	for(int i = 1; i <= b; i++) {
-		int sum =0;
+		  int sum =0;
 	
 		for (int s=1; s<=i; s++){
-   
+ 
            if ((i%s)==0) {
              sum ++;
            }
-    
+        }
             if(sum == 2) {
           cout << i << " ";
            }
           
-        } 
+        
     
 	}
     	

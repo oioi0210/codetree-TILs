01000ac1e248a03641f121b029a698c910df70a9
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-29 21:03:20 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240129/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int arr[9];
+	int sum =0;
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i];
+		if (i==2||i==4||i==9){
+			sum+=arr[i];
+		}
+	}
+	cout << sum ;
+
+	return 0;
+
+}

25c21681d8851e8f5fd085b7ce82f0d269006ed2
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-01-30 20:23:13 +0900

    Title: 특정 위치의 문자 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 문자 배열 선언
+    char arr[10];
+
+    // 10개의 문자를 배열에 입력받아 저장합니다.
+    for(int i = 0; i < 10; i++)
+        cin >> arr[i];
+
+    // 1번째, 4번째, 7번째 인덱스에 저장된 값 들을 차례대로 출력
+    cout << arr[1] << " " << arr[4] << " " << arr[7];
+
+    return 0;
+}

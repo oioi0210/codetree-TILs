02e36d1ac8d0dfdb36517691a5fc644e7845a15b
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-03-28 12:54:22 +0900

    Title: 점수대 파악하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240328/점수대 파악하기/find-out-the-score-range.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//배열 선언
+	int count_arr[11] = {};
+	int arr[100];
+	
+	// arr[i]를 입력받은 후 카운팅 배열을 통해 십의 자리수 각각의 빈도 저장, 0이 나오면 for문에서 빠져나오기
+	for(int i = 0; i <=100; i++) {
+		cin >> arr[i];
+		if(arr[i] == 0)
+			break;
+		if(arr[i] <= 10)
+			continue;
+		count_arr[arr[i] / 10]++;
+	}
+	
+    // 1부터 9까지 나온 횟수를 출력
+    for(int i = 10; i >= 1; i--) {
+        cout << i <<0<< " - " << count_arr[i] << endl;
+    }
+
+    return 0;
+}

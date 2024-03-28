2af36f6a4062c69af16b073cbf64ca1e6060d4d0
oioi0210 <154214807+oioi0210@users.noreply.github.com>
2024-03-28 12:19:46 +0900

    Title: 1-9 개수 세기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240328/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n=0;
+cin >> n;
+	int count_arr[10] = {};
+	int arr[100];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		countcount_arr[arr[i]]++;
+	}
+
+	// 개수 세기
+	for (int i = 1; i < 10; i++) {
+		count_arr[arr[i]] << endl;
+	}
+
+	return 0;
+
+}

61b74400c6a2f793ecd4093341327658db4711df
12211561 <154214807+oioi0210@users.noreply.github.com>
2024-04-11 13:09:59 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240411/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[100];
+    int a;
+    for (int i =n; i >0; i--) { 
+       arr[i] =a;
+       
+        if(a%2==0) {
+            cout << arr[i];
+            }
+
+
+    }
+    return 0;
+}

04c268a3162705a15974f9f24c7a607ef2458be6
12211561 <154214807+oioi0210@users.noreply.github.com>
2024-04-11 12:44:15 +0900

    Title: 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240411/개수 세기/count-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n>>m;
+	int arr[100];
+
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+		if (arr[i] == m) {
+			cnt++;
+		}
+	}
+
+	cout << cnt;
+	return 0;
+
+}

de71a903344d7e004f0df1f1fded0f4bf161ead4
12211561 <154214807+oioi0210@users.noreply.github.com>
2024-05-08 15:46:55 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 1084ms, Memory: 22MB, Status: Time Limit Exceed - CodeTree

---
+++ 240508/가장 왼쪽에 있는 최댓값/leftmost-max-value.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	
+	cin >> n;
+
+	int arr[100];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	int max{ 0 };
+	
+	int cnt = n;
+	int sun{ 0 };
+	while (cnt !=1) {
+		
+		for (int i = 0; i < cnt; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+				sun = i;
+			}
+		}
+		cnt = sun + 1;
+		
+		cout << cnt  << " ";
+		
+		max = 0;
+		cnt--;
+	
+	}
+	cout << 1;
+	return 0;
+}

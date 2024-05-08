dd405b286b1a6556663e869dc3c7453f9d6c0893
12211561 <154214807+oioi0210@users.noreply.github.com>
2024-05-08 16:56:50 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240508/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#define MAX_NUM 1000
+
+using namespace std;
+
+int main() {
+    // 변수 선언:
+    int n;
+    int price[MAX_NUM];
+
+    // 입력:
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> price[i];
+
+    // 배열을 앞에서부터 순회하며 사는 시점의 후보를 선택합니다
+    int max_profit = 0;
+    for(int i = 0; i < n; i++) {
+        // 사는 시점의 다음 해부터 순회하며 파는 시점의 후보를 선택합니다
+        for(int j = i + 1; j < n; j++) {
+			int profit = price[j] - price[i];
+            
+            if(profit > max_profit)
+                max_profit = profit;
+        }
+    }
+
+    cout << max_profit;
+    return 0;
+}

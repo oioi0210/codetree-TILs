73f545eea483e32eea5c0d7f07bdddcedafd9026
12211561 <154214807+oioi0210@users.noreply.github.com>
2024-05-09 15:21:26 +0900

    Title: 숫자로 이루어진 사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240509/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void PrintRect(int n) {
+    int a{1}
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++)
+            
+            cout << a;
+            a++;
+            if(a==10){
+                a=1;
+            }
+
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    PrintRect(n);
+    return 0;
+}

31a11049a1ad1140e101b68a42437183f15041a7
12211561 <154214807+oioi0210@users.noreply.github.com>
2024-05-22 15:49:12 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240522/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int arr_2d[4][4];
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			cin >> arr_2d[i][j];
+		}
+	}
+int sum{0};
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < i+1; j++) {
+			sum+= arr_2d[i][j];
+		}
+		
+	}
+	cout<< sum<< " ";
+        
+	
+
+	return 0;
+
+}
